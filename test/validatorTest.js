bf139ca29afe1fee5baaaf4a02e13de2fcd87458
Adriano Raiano <adriano@raiano.ch>
2014-08-28 14:40:20 +0200

    test validator

---
+++ test/validatorTest.js
@@ -0,0 +1,112 @@
+var expect = require('expect.js'),
+  validator = require('../lib/validator'),
+  tv4 = require('tv4');
+
+describe('validator', function () {
+  
+  describe('executing', function () {
+    
+    describe('without any arguments', function () {
+      
+      it('it should throw an error', function () {
+
+        expect(function () {
+          validator();
+        }).to.throwError(/tv4/);
+        
+      });
+      
+    });
+
+    describe('with wrong tv4 argument', function () {
+
+      it('it should throw an error', function () {
+
+        expect(function () {
+          validator({});
+        }).to.throwError(/tv4/);
+
+      });
+
+    });
+
+    describe('without schema argument', function () {
+
+      it('it should throw an error', function () {
+
+        expect(function () {
+          validator({ validateMultiple: function () {} });
+        }).to.throwError(/schema/);
+
+      });
+
+    });
+
+    describe('with all correct arguments', function () {
+
+      it('it should throw an error', function () {
+
+        expect(function () {
+          validator({ validateMultiple: function () {} }, {});
+        }).not.to.throwError();
+
+      });
+
+      it('it should return a function', function () {
+
+        var fn = validator({ validateMultiple: function () {} }, {});
+        expect(fn).to.be.a('function');
+        
+      });
+
+      describe('validating', function () {
+
+        var val = validator(tv4, {
+          "type": "object",
+          "properties": {
+            "firstName": {
+              "title": "First name",
+              "type": "string"
+            },
+            "lastName": {
+              "title": "Last name",
+              "type": "string"
+            }
+          },
+          "required": ["firstName"]
+        });
+
+        describe('a correct object', function () {
+
+          it('it should return null', function () {
+
+            var res = val({ firstName: 'First', lastName: 'Name' });
+            expect(res).to.eql(null);
+
+          });
+
+        });
+
+        describe('a wrong object', function () {
+
+          it('it should return a valiation error', function () {
+
+            var res = val({ lastName: 4 });
+            expect(res.name).to.eql('ValidationError');
+            expect(res.message).to.match(/missing/i);
+            expect(res.more).to.be.an('array');
+            expect(res.more.length).to.eql(2);
+            expect(res.more[1].message).to.match(/invalid/i);
+            expect(res.more[1].dataPath).to.match(/lastName/);
+
+          });
+
+        });
+        
+      });
+
+    });
+    
+  });
+
+});
