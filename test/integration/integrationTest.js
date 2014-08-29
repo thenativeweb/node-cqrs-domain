239f318bcc9b3887bfaa6ad05d3e4770a8c19199
Adriano Raiano <adriano@raiano.ch>
2014-08-29 19:47:40 +0200

    made some integration tests with 2 sets and 2 formats

--- test/integration/integrationTest.js
+++ test/integration/integrationTest.js
@@ -743,7 +743,7 @@ describe('integration', function () {
 
         describe('that fails on a business rule', function () {
 
-          it.only('it should publish a command rejected event and it should callback with an error and without events', function (done) {
+          it('it should publish a command rejected event and it should callback with an error and without events', function (done) {
 
             var publishedEvents = [];
 
@@ -840,7 +840,7 @@ describe('integration', function () {
               },
               head: {
                 userId: 'userId',
-                revision: 0
+                revision: 1
               }
             };
 
@@ -905,8 +905,260 @@ describe('integration', function () {
       });
 
     });
-    
-    
+
+    describe('format 1', function () {
+
+      var domain;
+
+      before(function (done) {
+        domain = api({ domainPath: __dirname + '/fixture/set2', commandRejectedEventName: 'rejectedCommand' });
+        domain.defineCommand({
+          id: 'id',
+          name: 'name',
+          aggregateId: 'aggregate.id',
+          context: 'context.name',
+          aggregate: 'aggregate.name',
+          payload: 'payload',
+          revision: 'revision',
+          version: 'version',
+          meta: 'meta'
+        });
+        domain.defineEvent({
+          correlationId: 'correlationId',
+          id: 'id',
+          name: 'name',
+          aggregateId: 'aggregate.id',
+          context: 'context.name',
+          aggregate: 'aggregate.name',
+          payload: 'payload',
+          revision: 'revision',
+          version: 'version',
+          meta: 'meta'
+        });
+
+        domain.init(done);
+      });
+
+      describe('handling a command that will not be handled', function () {
+
+        it('it should not publish any event and it should callback with an error and without events', function (done) {
+
+          var publishedEvents = [];
+
+          domain.onEvent(function (evt) {
+            publishedEvents.push(evt);
+          });
+
+          var cmd = {
+            id: 'cmdId',
+            name: 'cmdName',
+            aggregate: {
+              id: 'aggregateId',
+              name: 'aggregate'
+            },
+            context: {
+              name: 'context'
+            },
+            payload: 'payload',
+            revision: 0,
+            version: 0,
+            meta: {
+              userId: 'userId'
+            }
+          };
+
+          domain.handle(cmd, function (err, evts) {
+            expect(err).to.be.ok();
+            expect(err.message).to.match(/found/i);
+            expect(evts).not.to.be.ok();
+            expect(publishedEvents.length).to.eql(0);
+
+            done();
+          });
+
+        });
+
+      });
+
+      describe('handling a command with correct command name but wrong aggregate and context', function () {
+
+        it('it should not publish any event and it should callback with an error and without events', function (done) {
+
+          var publishedEvents = [];
+
+          domain.onEvent(function (evt) {
+            publishedEvents.push(evt);
+          });
+
+          var cmd = {
+            id: 'cmdId',
+            name: 'enterNewPerson',
+            aggregate: {
+              id: 'aggregateId',
+              name: 'aggregate'
+            },
+            context: {
+              name: 'context'
+            },
+            payload: 'payload',
+            revision: 0,
+            version: 0,
+            meta: {
+              userId: 'userId'
+            }
+          };
+
+          domain.handle(cmd, function (err, evts) {
+            expect(err).to.be.ok();
+            expect(err.message).to.match(/found/i);
+            expect(evts).not.to.be.ok();
+            expect(publishedEvents.length).to.eql(0);
+
+            done();
+          });
+
+        });
+
+      });
+
+      describe('handling a command with correct command name and correct aggregate but wrong context', function () {
+
+        it('it should not publish any event and it should callback with an error and without events', function (done) {
+
+          var publishedEvents = [];
+
+          domain.onEvent(function (evt) {
+            publishedEvents.push(evt);
+          });
+
+          var cmd = {
+            id: 'cmdId',
+            name: 'enterNewPerson',
+            aggregate: {
+              id: 'aggregateId',
+              name: 'person'
+            },
+            context: {
+              name: 'context'
+            },
+            payload: 'payload',
+            revision: 0,
+            version: 0,
+            meta: {
+              userId: 'userId'
+            }
+          };
+
+          domain.handle(cmd, function (err, evts) {
+            expect(err).to.be.ok();
+            expect(err.message).to.match(/found/i);
+            expect(evts).not.to.be.ok();
+            expect(publishedEvents.length).to.eql(0);
+
+            done();
+          });
+
+        });
+
+      });
+
+      describe('handling a command', function () {
+
+        describe('that does fails on the validation rules of a parent schema', function () {
+
+          it('it should publish a command rejected event and it should callback with an error and without events', function (done) {
+
+            var publishedEvents = [];
+
+            domain.onEvent(function (evt) {
+              publishedEvents.push(evt);
+            });
+
+            var cmd = {
+              idd: 'cmdId',
+              name: 'enterNewPerson',
+              aggregate: {
+                id: 'aggregateId',
+                name: 'person'
+              },
+//              context: {
+//                name: 'hr'
+//              },
+              payload: {
+                firstname: 'jack',
+                lastname: 'doe',
+                email: 'jack'
+              },
+              revision: 0,
+              version: 0,
+              meta: {
+                userId: 'userId'
+              }
+            };
+
+            domain.handle(cmd, function (err, evts) {
+              expect(err).to.be.ok();
+              expect(err.name).to.eql('ValidationError');
+              expect(evts).not.to.be.ok();
+              expect(publishedEvents.length).to.eql(1);
+              expect(publishedEvents[0].name).to.eql('rejectedCommand');
+              expect(publishedEvents[0].payload.reason.name).to.eql('ValidationError');
+
+              done();
+            });
+
+          });
+
+        });
+
+        describe('that has a custom command handler', function () {
+
+          it('it use that handler', function (done) {
+
+            var publishedEvents = [];
+
+            domain.onEvent(function (evt) {
+              publishedEvents.push(evt);
+            });
+
+            var cmd = {
+              id: 'cmdId',
+              name: 'enterNewSpecialPerson',
+              aggregate: {
+                id: 'aggregateId',
+                name: 'person'
+              },
+//              context: {
+//                name: 'hr'
+//              },
+              payload: {
+              },
+              revision: 1,
+              version: 0,
+              meta: {
+                userId: 'userId'
+              }
+            };
+
+            domain.handle(cmd, function (err, evts) {
+              expect(err).not.to.be.ok();
+              expect(evts.length).to.eql(1);
+              expect(evts[0].my).to.eql('special');
+              expect(evts[0].ev).to.eql('ent');
+              expect(publishedEvents.length).to.eql(1);
+              expect(publishedEvents[0].my).to.eql('special');
+              expect(publishedEvents[0].ev).to.eql('ent');
+
+              done();
+            });
+
+          });
+
+        });
+
+      });
+
+    });
 
   });
 
